2a70dc3c8dc9c48171e0a76d0574c4cce8c9fc15
UO269509 <60443410+UO269509@users.noreply.github.com>
2021-06-09 15:50:09 +0200

    Add files via upload

--- Proyecto_SEW/js/procesarXML.js
+++ Proyecto_SEW/js/procesarXML.js
@@ -11,32 +11,35 @@ class Procesar {
             var file = document.getElementById("subirArchivos").files[0];
             var reader = new FileReader();
             reader.onload = function (e) {
+                var htmlText = "";
                 var xmlDoc = $.parseXML(reader.result);
                 var reseñas = $(xmlDoc).find("reseña");
                 var counter = 0;
                 for( var i = 1; i < reseñas.length + 1; i++)
                 {
                     var reseña = reseñas[i - 1];
-                    $(document.getElementById(i)).before("<h1>"+ reseña.attributes[0].nodeValue + "</h1>");
-                    //$("#viaje" + i).append(reseña.attributes[0].nodeValue);
-                    $("#nombre" + i).append("Nombre: " + reseña.children[0].innerHTML);
-                    $("#correo" + i).append("Correo: " + reseña.children[1].innerHTML);
-                    $("#procedencia" + i).append("Procedencia: " + reseña.children[2].innerHTML);
-                    $("#hotel" + i).append("Hotel: " + reseña.children[3].innerHTML);
-                    $("#puntuacion_hotel" + i).append("Puntuación del hotel: " + reseña.children[4].innerHTML);
-                    $("#reseña" + i).append("Reseña: " + reseña.children[5].innerHTML);
-                    $("#lugares" + i).append("Lugares:");
+                    htmlText += "<h1>" + reseña.attributes[0].nodeValue + "</h1>";
+                    htmlText += "<ul>";
+                    htmlText += "<li>"+ "Nombre: " + reseña.children[0].innerHTML + "</li>";
+                    htmlText += "<li>"+ "Correo: " + reseña.children[1].innerHTML + "</li>";
+                    htmlText += "<li>"+ "Procedencia: " + reseña.children[2].innerHTML + "</li>";
+                    htmlText += "<li>"+ "Hotel: " + reseña.children[3].innerHTML + "</li>";
+                    htmlText += "<li>"+ "Puntuación del hotel: " + reseña.children[4].innerHTML + "</li>";
+                    htmlText += "<li>"+ "Reseña: " + reseña.children[5].innerHTML + "</li>";
+                    htmlText += "<li>"+ "Lugares:" + "</li>";
                     var lugares = $(xmlDoc).find("lugar");
                     for( var j = 1; j < lugares.length - 1; j++)
                     {
                         var lugar = lugares[counter++];
-                        $("#nombre_lugar" + i + j).append(lugar.attributes[0].nodeValue);
-                        $("#reseña_lugar" + i + j).append("Reseña: " + lugar.children[0].innerHTML);
-                        $("#puntuacion_lugar" + i + j).append("Puntuación: " + lugar.children[1].innerHTML);
+                        htmlText += "<li class='lugares'>"+ lugar.attributes[0].nodeValue + "</li>";
+                        htmlText += "<li>"+ "Reseña: " + lugar.children[0].innerHTML + "</li>";
+                        htmlText += "<li>"+ "Puntuación: " + lugar.children[1].innerHTML + "</li>";
                         var imagen = "<img src='../images/" + lugar.children[2].innerHTML + "' alt='Imagen del paisaje' height='205' width='512' />";
-                        $("#imagen" + i + j).append(imagen);
+                        htmlText += "<li>"+ imagen + "</li>";
                     }
+                    htmlText += "</ul>";
                 }
+                $("#inputFile").after(htmlText);
             };
             reader.readAsText(file);
         }else{
